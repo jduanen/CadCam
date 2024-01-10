26709a67ff2ab238675a58f1358d62bd15466a57
Duane Northcutt <duane.northcutt@gmail.com>
2024-01-09 20:34:07 -0800

    Updated.

--- FatMan/src/scadGen.py
+++ FatMan/src/scadGen.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
 '''
-????
+################################################################################
+# ????
+#
+# https://en.wikibooks.org/wiki/OpenSCAD_User_Manual
+#
+# polyhedron(points: Sequence[Tuple[float, float, float]], faces: Union[Sequence[int], Sequence[Sequence[int]]], convexity: int = 10, triangles: Union[Sequence[int], Sequence[Sequence[int]]] = None) -> None
+# rotate([x, y, z])
+# translate([x, y, z])
+################################################################################
 '''
 
-import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import art3d
+import numpy as np
+from pprint import pprint
+from solid import polyhedron, rotate, translate, scad_render_to_file
 
-from TruncatedIcosohedron.HexagonalPyramid import HexagonalPyramid as Hex
+from TruncatedIcosohedron.PolygonalPyramid import PolygonalPyramid
 
 
-RADIUS_BOOST = 463.55
-RADIUS_LENS = 688.975
+BOOST_RADIUS = 463.55
+LENS_RADIUS = 688.975
 
 
-def run(options):
-    hexBoost = Hex(RADIUS_BOOST)
-    hexLens = Hex(RADIUS_LENS)
-
-    h = hexBoost.getDict()
-    print(h['vertices'])
-
+def wireframeVis(vertices):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    x, y, z = zip(*h['vertices'])
+    x, y, z = zip(*vertices)
     ax.scatter(x, y, z, marker='o')
 
     sides = len(x) - 1
@@ -39,6 +44,51 @@ def run(options):
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     plt.show()
+
+def facesVis(vertices, faces):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    xmax = ymax = zmax = 500
+    xmin = ymin = zmin = -500
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+    ax.set_zlim(zmin, zmax)
+
+    C = np.array([1,2,3,4,5,6,7])
+    norm = plt.Normalize(C.min(), C.max())
+    colors = plt.cm.viridis(norm(C))
+
+    fs = [[vertices[p] for p in f] for f in faces]
+    pc = art3d.Poly3DCollection(fs, facecolors=colors)
+    ax.add_collection(pc)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
+
+def makePentPoly(radius):
+    p = PolygonalPyramid(5, radius)
+    if False:
+        wireframeVis(p.vertices)
+    if False:
+        facesVis(p.vertices, p.faces)
+    return polyhedron(points=p.vertices, faces=p.faces)
+
+def makeHexPoly(radius):
+    h = PolygonalPyramid(6, radius)
+    if True:
+        wireframeVis(h.vertices)
+    if False:
+        facesVis(h.vertices, h.faces)
+    return polyhedron(points=h.vertices, faces=h.faces)
+
+def run(options):
+    boostHexPoly = makeHexPoly(BOOST_RADIUS)
+#    boostPentPoly = makePentPoly(BOOST_RADIUS)
+
+    geomObj = boostHexPoly #+ boostPentPoly
+    scad_render_to_file(geomObj, 'output.scad')
 
 def getOps():
     opts = {}
