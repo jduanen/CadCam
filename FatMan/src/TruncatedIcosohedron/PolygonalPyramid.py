91d707305558748c6f4f3a802098683d7bf1ea56
Duane Northcutt <duane.northcutt@gmail.com>
2024-01-17 15:32:11 -0800

    Checkpoint.

--- FatMan/src/TruncatedIcosohedron/PolygonalPyramid.py
+++ FatMan/src/TruncatedIcosohedron/PolygonalPyramid.py
@@ -20,12 +20,15 @@ class PolygonalPyramid():
         self.sides = sides
         self.radius = radius
 
+        # edge length of truncated icosahedron with given radius of circumscribed sphere
+        # R = e * sqrt(58 + 18 * sqrt(5)) / 4
+        # e = R * 4 / sqrt(58 + 18 * sqrt(5))
         self.edgeSize = self.radius / (sqrt(58 + 18 * sqrt(5)) / 4)
         self.vertices = []
         self.faces = []
 
         # radius of circle that inscribes the base of the regular polygon:
-        #  c(r) = a(r) / 2 * sin(pi / 5)
+        #  c(r) = a(r) / (2 * sin(pi / s))
         self.circle = self.edgeSize / (2 * sin(pi / self.sides))
 
         # (z-axis) height of pyramid: H(r) = sqrt(r**2 - c(r)**2)
@@ -39,16 +42,11 @@ class PolygonalPyramid():
         self.vertices.append([0, 0, 0])
         self.faces = [[self.sides, i, (i + 1) % self.sides]
                       for i in range(0, self.sides)]
-        self.faces.append(list(range(0, self.sides)))
+        self.faces.append(list(range(self.sides-1, -1, -1))) # make order of points be clockwise
 
     def __str__(self):
-        return self._getString()
+        return f"Sides: {self.sides}, Radius: {self.radius}, Circle: {self.circle}, Pyramid Height: {self.height}, EdgeSize: {self.edgeSize}, Vertices: {str(self.vertices)}, Faces: {str(self.faces)}"
 
     def __repr__(self):
-        return str(self._getDict())
-
-    def _getString(self):
-        return f"Radius: {self.radius}, Sides: {self.sides}, Pyramid Height: {self.height}, EdgeSize: {self.edgeSize}, Vertices: {str(self.vertices)}, Faces: {str(self.faces)}"
-
-    def _getDict(self):
-        return {'radius': self.radius, 'sides': self.sides, 'height': self.height, 'edgeSize': self.edgeSize, 'vertices': self.vertices, 'faces': self.faces}
+        d = {'sides': self.sides, 'radius': self.radius, 'circle': {self.circle}, 'height': self.height, 'edgeSize': self.edgeSize, 'vertices': self.vertices, 'faces': self.faces}
+        return str(d)
--- FatMan/src/scadGen.py
+++ FatMan/src/scadGen.py
@@ -4,24 +4,49 @@
 # ????
 #
 # https://en.wikibooks.org/wiki/OpenSCAD_User_Manual
+# https://en.wikibooks.org/wiki/OpenSCAD_User_Manual/Transformations#color
 #
-# polyhedron(points: Sequence[Tuple[float, float, float]], faces: Union[Sequence[int], Sequence[Sequence[int]]], convexity: int = 10, triangles: Union[Sequence[int], Sequence[Sequence[int]]] = None) -> None
-# rotate([x, y, z])
-# translate([x, y, z])
+# polyhedron(points: Sequence[Tuple[float, float, float]], faces: solid.union[Sequence[int], Sequence[Sequence[int]]], convexity: int = 10, triangles: solid.union[Sequence[int], Sequence[Sequence[int]]] = None) -> None
+# solid.rotate([x, y, z]): solid.rotates z-axis, then y-axis, and finally x-axis
+# solid.translate([x, y, z])
+#
+# $fn=128
+#
 ################################################################################
 '''
 
+from math import acos, degrees, pi, sqrt, tan
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import art3d
 import numpy as np
 from pprint import pprint
-from solid import polyhedron, rotate, translate, scad_render_to_file
+import solid
 
 from TruncatedIcosohedron.PolygonalPyramid import PolygonalPyramid
 
+FELT_THICKNESS = 1.5875
+CORK_THICKNESS = 12.7
 
-BOOST_RADIUS = 463.55
-LENS_RADIUS = 688.975
+LARGE_RADIUS = 800.0    # diameter = 1,600.0
+LENS_RADIUS = 688.975   # diameter = 1,377.95
+LENS_CORK_RADIUS = LENS_RADIUS + CORK_THICKNESS
+BOOST_RADIUS = 463.55   # diameter =   927.1
+BOOST_FELT_RADIUS = BOOST_RADIUS + FELT_THICKNESS
+PUSHER_RADIUS = 234.95  # diameter =   469.9
+PUSHER_FELT_RADIUS = PUSHER_RADIUS + FELT_THICKNESS
+
+X_RADIUS = LARGE_RADIUS
+
+# Hexagon-Hexagon Angle: degrees(acos(−sqrt(5) / 3)) ≈ 138.189685104 deg
+HEX_HEX_ANGLE = degrees(acos(-sqrt(5) / 3))
+
+# Hexagon-Pentagon Angle: degrees(acos(−sqrt(15 * (5 + 2 * sqrt(5))) / 15)) ≈ 142.622631859 deg
+HEX_PENT_ANGLE = degrees(acos(-sqrt(15 * (5 + 2 * sqrt(5))) / 15))
+
+#### FIXME where did this come from?
+TI_DIHEDRAL_ANGLE = 116.565
+## 180 - 116.565 = 63.435
+TI_DIHEDRAL_COMP_ANGLE = 180 - TI_DIHEDRAL_ANGLE # 63.435
 
 
 def wireframeVis(vertices):
@@ -56,10 +81,10 @@ def facesVis(vertices, faces):
 
     C = np.array([1,2,3,4,5,6,7])
     norm = plt.Normalize(C.min(), C.max())
-    colors = plt.cm.viridis(norm(C))
+    solid.colors = plt.cm.viridis(norm(C))
 
     fs = [[vertices[p] for p in f] for f in faces]
-    pc = art3d.Poly3DCollection(fs, facecolors=colors)
+    pc = art3d.Poly3DCollection(fs, facecolors=solid.colors)
     ax.add_collection(pc)
 
     ax.set_xlabel('X')
@@ -67,28 +92,104 @@ def facesVis(vertices, faces):
     ax.set_zlabel('Z')
     plt.show()
 
-def makePentPoly(radius):
+def makePentPoly(radius, color="Red"):
     p = PolygonalPyramid(5, radius)
+    '''
+    #pprint(f"Pent: {p}")
     if False:
         wireframeVis(p.vertices)
     if False:
         facesVis(p.vertices, p.faces)
-    return polyhedron(points=p.vertices, faces=p.faces)
+    '''
+    pent = solid.polyhedron(points=p.vertices, faces=p.faces)
+    if color is not None:
+        pent = solid.color(color)(pent)
+    return pent
 
-def makeHexPoly(radius):
+def makeHexPoly(radius, color="Green"):
     h = PolygonalPyramid(6, radius)
-    if True:
+    '''
+    #pprint(f"Hex: {h}")
+    if False:
         wireframeVis(h.vertices)
     if False:
         facesVis(h.vertices, h.faces)
-    return polyhedron(points=h.vertices, faces=h.faces)
+    '''
+    hexa = solid.polyhedron(points=h.vertices, faces=h.faces)
+    if color is not None:
+        hexa = solid.color(color)(hexa)
+    return hexa
 
 def run(options):
-    boostHexPoly = makeHexPoly(BOOST_RADIUS)
-#    boostPentPoly = makePentPoly(BOOST_RADIUS)
+    deltaVec = lambda vec1, vec2: [sum(x) for x in zip(vec1, vec2)]
 
-    geomObj = boostHexPoly #+ boostPentPoly
-    scad_render_to_file(geomObj, 'output.scad')
+    pusher = solid.color("yellow")(solid.sphere(PUSHER_RADIUS))
+    pusherFelt = solid.color("gray")(solid.sphere(PUSHER_FELT_RADIUS)) - pusher
+
+    boostPolys = {}
+    boostPolys['pent'] = makePentPoly(X_RADIUS, color="Red")
+    geomObj = [boostPolys['pent']]
+    boostPolys['pentB'] = solid.rotate(a=[0, 180, 0])(makePentPoly(X_RADIUS, color="Red"))
+    geomObj += boostPolys['pentB']
+
+    '''
+    HEX0_ROT  = [36,   0, -18]  #### FIXME
+    PENT0_ROT = [ 0, 300,  36]  #### FIXME
+    HEX1_ROT  = [72,   0, -18]  #### FIXME
+    '''
+    for i in range(0, 5):
+        angle = i * (360.0 / 5)
+        hexName = f"hex0_{i}"
+        boostPolys[hexName] = solid.rotate(a=[0, HEX_PENT_ANGLE, angle])(
+                                     solid.rotate(a=[0, 0, 30])(
+                                            makeHexPoly(X_RADIUS)))
+        geomObj += boostPolys[hexName]
+        hexName += "B"
+        boostPolys[hexName] = solid.rotate(a=[0, HEX_PENT_ANGLE, angle])(
+                                     solid.rotate(a=[180, 0, 30])(
+                                            makeHexPoly(X_RADIUS)))
+        geomObj += boostPolys[hexName]
+
+        pentName = f"pent0_{i}"
+        val = TI_DIHEDRAL_COMP_ANGLE
+        boostPolys[pentName] = solid.rotate(a=[0, val, angle])(
+                                      solid.rotate(a=[0, 0, 180])(
+                                             makePentPoly(X_RADIUS)))
+        geomObj += boostPolys[pentName]
+        pentName += "B"
+        boostPolys[pentName] = solid.rotate(a=[0, val+180, angle])(
+                                      solid.rotate(a=[0, 0, 180])(
+                                             makePentPoly(X_RADIUS)))
+        geomObj += boostPolys[pentName]
+
+        hexName = f"hex1_{i}"
+        X_ANGLE = 101  #### FIXME
+        boostPolys[hexName] = solid.rotate(a=[0, X_ANGLE, angle])(
+                                     solid.rotate(a=[0, 0, 30])(
+                                            makeHexPoly(X_RADIUS)))
+        geomObj += boostPolys[hexName]
+
+        hexName = f"hex2_{i}"
+        #### FIXME
+        boostPolys[hexName] = solid.rotate(a=[0, X_ANGLE-180, angle])(
+                                     solid.rotate(a=[0, 0, 30])(
+                                            makeHexPoly(X_RADIUS)))
+        geomObj += boostPolys[hexName]
+
+    boost = solid.color("Salmon")(solid.sphere(X_RADIUS))
+    boostFelt = solid.color("gray")(solid.sphere(BOOST_FELT_RADIUS)) - boost
+
+    lens = solid.color("Lime")(solid.sphere(LENS_RADIUS))
+    lensCork = solid.color("Tan")(solid.sphere(LENS_CORK_RADIUS)) - lens
+
+    obj = solid.union()(lensCork, boostFelt, geomObj, pusherFelt, pusher)
+    if False:
+        planeOrigin = [0, -1000, 1000]
+        planeNormal = [0, 1, 0]
+        cutPlane = solid.translate(planeOrigin)(solid.rotate(a=90, v=planeNormal)(solid.cube([2000,2000,2000])))
+        obj = solid.difference()(obj, cutPlane)
+
+    solid.scad_render_to_file(obj, 'ico.scad')
 
 def getOps():
     opts = {}
@@ -97,4 +198,29 @@ def getOps():
 if __name__ == '__main__':
     opts = getOps()
     run(opts)
-    print("DONE")
+#    print("DONE")
+
+'''
+##    geomObj += solid.sphere(BOOST_RADIUS)
+
+#    geomObj += solid.rotate(a=[90,0,0])(solid.circle(r = BOOST_RADIUS))
+#    geomObj += solid.rotate(a=[90,0,108])(solid.circle(r = BOOST_RADIUS)
+
+#    geomObj += solid.rotate(a=[90,0,216])(solid.circle(r = BOOST_RADIUS))
+
+    polys = []
+    p = PolygonalPyramid(5, BOOST_RADIUS)
+    polys.append(polyhedron(points=p.vertices, faces=p.faces))
+
+#    h = PolygonalPyramid(6, BOOST_RADIUS)
+#    hexa = polyhedron(points=h.vertices, faces=h.faces)
+
+#    pent = makePentPoly(BOOST_RADIUS)
+#    pentB = solid.rotate(a=[0, 180, 0])(pent)
+    polys.append(solid.rotate(a=[0, 180, 0])(makePentPoly(BOOST_RADIUS)))
+    s = solid.sphere(20);
+#    polys = [pentA, pentB]
+    obj = solid.union()([s, *polys])
+'''
+#    obj = solid.union()([pusher, geomObj])
+#    c = solid.translate([750,0,0])(solid.cube([1500,1500,1500], center=True))
