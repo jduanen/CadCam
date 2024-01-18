40c97883251777ec075725e0d8731ed8c7d8cbb6
Duane Northcutt <duane.northcutt@gmail.com>
2024-01-17 17:01:43 -0800

    First cut.

--- FatMan/src/scadGen.py
+++ FatMan/src/scadGen.py
@@ -126,55 +126,32 @@ def run(options):
     pusher = solid.color("yellow")(solid.sphere(PUSHER_RADIUS))
     pusherFelt = solid.color("gray")(solid.sphere(PUSHER_FELT_RADIUS)) - pusher
 
-    boostPolys = {}
-    boostPolys['pent'] = makePentPoly(X_RADIUS, color="Red")
-    geomObj = [boostPolys['pent']]
-    boostPolys['pentB'] = solid.rotate(a=[0, 180, 0])(makePentPoly(X_RADIUS, color="Red"))
-    geomObj += boostPolys['pentB']
+    geomObj = makePentPoly(X_RADIUS, color="Red")
+    geomObj += solid.rotate(a=[0, 180, 0])(makePentPoly(X_RADIUS, color="Red"))
 
-    '''
-    HEX0_ROT  = [36,   0, -18]  #### FIXME
-    PENT0_ROT = [ 0, 300,  36]  #### FIXME
-    HEX1_ROT  = [72,   0, -18]  #### FIXME
-    '''
     for i in range(0, 5):
         angle = i * (360.0 / 5)
-        hexName = f"hex0_{i}"
-        boostPolys[hexName] = solid.rotate(a=[0, HEX_PENT_ANGLE, angle])(
-                                     solid.rotate(a=[0, 0, 30])(
-                                            makeHexPoly(X_RADIUS)))
-        geomObj += boostPolys[hexName]
-        hexName += "B"
-        boostPolys[hexName] = solid.rotate(a=[0, HEX_PENT_ANGLE, angle])(
-                                     solid.rotate(a=[180, 0, 30])(
-                                            makeHexPoly(X_RADIUS)))
-        geomObj += boostPolys[hexName]
+        geomObj += solid.rotate(a=[0, HEX_PENT_ANGLE, angle])(
+                                    solid.rotate(a=[0, 0, 30])(
+                                        makeHexPoly(X_RADIUS)))
+        geomObj += solid.rotate(a=[0, HEX_PENT_ANGLE, angle])(
+                                    solid.rotate(a=[180, 0, 30])(
+                                        makeHexPoly(X_RADIUS)))
 
-        pentName = f"pent0_{i}"
-        val = TI_DIHEDRAL_COMP_ANGLE
-        boostPolys[pentName] = solid.rotate(a=[0, val, angle])(
-                                      solid.rotate(a=[0, 0, 180])(
-                                             makePentPoly(X_RADIUS)))
-        geomObj += boostPolys[pentName]
-        pentName += "B"
-        boostPolys[pentName] = solid.rotate(a=[0, val+180, angle])(
-                                      solid.rotate(a=[0, 0, 180])(
-                                             makePentPoly(X_RADIUS)))
-        geomObj += boostPolys[pentName]
+        geomObj += solid.rotate(a=[0, TI_DIHEDRAL_COMP_ANGLE, angle])(
+                                    solid.rotate(a=[0, 0, 180])(
+                                        makePentPoly(X_RADIUS)))
+        geomObj += solid.rotate(a=[0, TI_DIHEDRAL_COMP_ANGLE, angle])(
+                                    solid.rotate(a=[0, 180, 180])(
+                                        makePentPoly(X_RADIUS)))
 
-        hexName = f"hex1_{i}"
-        X_ANGLE = 101  #### FIXME
-        boostPolys[hexName] = solid.rotate(a=[0, X_ANGLE, angle])(
-                                     solid.rotate(a=[0, 0, 30])(
-                                            makeHexPoly(X_RADIUS)))
-        geomObj += boostPolys[hexName]
-
-        hexName = f"hex2_{i}"
-        #### FIXME
-        boostPolys[hexName] = solid.rotate(a=[0, X_ANGLE-180, angle])(
-                                     solid.rotate(a=[0, 0, 30])(
-                                            makeHexPoly(X_RADIUS)))
-        geomObj += boostPolys[hexName]
+        X_ANGLE = HEX_PENT_ANGLE + HEX_HEX_ANGLE
+        geomObj += solid.rotate(a=[0, X_ANGLE, angle])(
+                                    solid.rotate(a=[0, 0, 30])(
+                                        makeHexPoly(X_RADIUS)))
+        geomObj += solid.rotate(a=[0, X_ANGLE, angle])(
+                                    solid.rotate(a=[180, 0, 30])(
+                                        makeHexPoly(X_RADIUS)))
 
     boost = solid.color("Salmon")(solid.sphere(X_RADIUS))
     boostFelt = solid.color("gray")(solid.sphere(BOOST_FELT_RADIUS)) - boost
@@ -188,6 +165,7 @@ def run(options):
         planeNormal = [0, 1, 0]
         cutPlane = solid.translate(planeOrigin)(solid.rotate(a=90, v=planeNormal)(solid.cube([2000,2000,2000])))
         obj = solid.difference()(obj, cutPlane)
+    obj = solid.union()(obj)
 
     solid.scad_render_to_file(obj, 'ico.scad')
 
@@ -198,29 +176,3 @@ def getOps():
 if __name__ == '__main__':
     opts = getOps()
     run(opts)
-#    print("DONE")
-
-'''
-##    geomObj += solid.sphere(BOOST_RADIUS)
-
-#    geomObj += solid.rotate(a=[90,0,0])(solid.circle(r = BOOST_RADIUS))
-#    geomObj += solid.rotate(a=[90,0,108])(solid.circle(r = BOOST_RADIUS)
-
-#    geomObj += solid.rotate(a=[90,0,216])(solid.circle(r = BOOST_RADIUS))
-
-    polys = []
-    p = PolygonalPyramid(5, BOOST_RADIUS)
-    polys.append(polyhedron(points=p.vertices, faces=p.faces))
-
-#    h = PolygonalPyramid(6, BOOST_RADIUS)
-#    hexa = polyhedron(points=h.vertices, faces=h.faces)
-
-#    pent = makePentPoly(BOOST_RADIUS)
-#    pentB = solid.rotate(a=[0, 180, 0])(pent)
-    polys.append(solid.rotate(a=[0, 180, 0])(makePentPoly(BOOST_RADIUS)))
-    s = solid.sphere(20);
-#    polys = [pentA, pentB]
-    obj = solid.union()([s, *polys])
-'''
-#    obj = solid.union()([pusher, geomObj])
-#    c = solid.translate([750,0,0])(solid.cube([1500,1500,1500], center=True))
