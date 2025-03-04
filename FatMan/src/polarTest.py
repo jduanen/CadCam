d4c9f153f390340c96e785362eb0bd46f4fb38cf
Duane Northcutt <duane.northcutt@gmail.com>
2025-03-04 12:35:06 -0800

    Fixed equations.

--- FatMan/src/polarTest.py
+++ FatMan/src/polarTest.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
-from math import sin, cos, pi
+from math import asin, cos, degrees, pi, sin, sqrt
 import matplotlib.pyplot as plt
+from matplotlib.patches import Arc
 import numpy as np
 from pprint import pprint
 import sys
@@ -26,32 +27,26 @@ LENS_FELT_GAP = 2
 NUM_STEPS = 100
 Y_OFFSET = 100
 
+HEX_ANGLE = asin(4 / sqrt(58 + (18 * sqrt(5))))  # half apical angle of a hexagonal pyramid
+PENT_ANGLE = asin((2 * sqrt(50 + (10 * sqrt(5)))) / (5 * sqrt(58 + (18 * sqrt(5)))))  # half apical angle of a pentagonal pyramid
+
+FOCUS_1 = (0, 0)
+FOCUS_2 = (0, LENS_OUTER_RADIUS)
+
 R_a = LENS_INNER_RADIUS
 R_l = LENS_OUTER_RADIUS
 
 
 # Equation A: cartesian [Richard]
 def aCart(x, y):
-  return ((N * (np.sqrt(x**2 + y**2) - R_a)) + 
-          (M * (np.sqrt(x**2 + (R_l - y)**2))))
-
-'''
-##(N ∗ (sqrt(x^2+y^2) − Ra)) + (M ∗ sqrt(x^2 + y^2 + Rl^2 + 2 ∗ Rl ∗ y))
-
-##((N * (np.sqrt(x**2 + y**2) - R_a)) + 
-## (M * (np.sqrt(x**2 + y**2 - (2 * R_l * y) + R_l**2))))
-
-# Equation A: Cartesian [from EqAPolar via Perplexity]
-def aCartP(x, y):
-  return ((N * (np.sqrt(x**2 + y**2) - R_a)) + 
-          (M * (np.sqrt(x**2 + y**2  + R_l**2 + 2 * R_l * y))))
-'''
+    return ((N * (np.sqrt(x**2 + y**2) - R_a)) + 
+            (M * (np.sqrt(x**2 + (R_l - y)**2))))
 
 # Equation A: polar [Richard]
 def aPolar(r, theta):
     return ((N * (r - R_a)) + 
             (M * np.sqrt((r**2 * np.cos(theta)**2) + 
-                         R_l**2 + 
+                         R_l**2 - 
                          (2 * r * R_l * np.sin(theta)) +
                          (r**2 * np.sin(theta)**2))))
 
@@ -62,75 +57,107 @@ def bPolar(r, theta, k):
     c = (k**2 + (N * R_a**2) + (2 * N * k * R_a) - (M**2 * R_l**2))
     return (r**2 - ((2 * (a + (b * np.cos(theta - (np.pi/2))))) * r) + c**2)
 
-def toCart(radius, angle, yOffset):
-  x = radius * np.cos(angle)
-  y = (radius * np.sin(angle)) + yOffset
-  return x, y
+def toCart(radius, angle):
+    x = radius * np.cos(angle)
+    y = (radius * np.sin(angle))
+    return x, y
 
 def polarPlot(ax, options):
-    r = Y_OFFSET
-    theta = -pi/2
+    r = R_l - Y_OFFSET
+    theta = pi/2
     k = aPolar(r, theta)
     if options['verbose']:
-      print(f"k={k} Polar")
+        print(f"k={k}; r={r}, theta={theta} Polar")
 
     if options['apex']:
-      x, y = toCart(r, theta, R_l)
-      plt.plot(x, y, k, "*", color="cyan", label='polar apex')
+        x, y = toCart(r, theta)
+        plt.plot(x, y, k, "*", color="cyan", label='polar apex')
 
-    theta = np.linspace(0, -(np.pi / 2), NUM_STEPS)
-    r = np.linspace(0, (R_l / 2), NUM_STEPS)
+    theta = np.linspace(0, (np.pi / 2), NUM_STEPS)
+    r = np.linspace(0, R_l, NUM_STEPS)
     R, THETA = np.meshgrid(r, theta)
     Z = aPolar(R, THETA)
 
-    if True:
-      xx = np.linspace(0, (R_l / 2), 100)
-      yy = np.linspace(0, R_l, 100)
-      XX, YY = np.meshgrid(xx, yy)
-      ZZ = np.full_like(XX, k)
-      ax.plot_surface(XX, YY, ZZ, color='gray', alpha=1)
+    if False:
+        xx = np.linspace(0, (R_l / 2), 100)
+        yy = np.linspace(0, R_l, 100)
+        XX, YY = np.meshgrid(xx, yy)
+        ZZ = np.full_like(XX, k)
+        ax.plot_surface(XX, YY, ZZ, color='gray', alpha=.5)
 
-    X, Y = toCart(R, THETA, R_l)
+    X, Y = toCart(R, THETA)
     if options['surface']:
-      surf1 = ax.plot_surface(X, Y, Z, cmap='viridis')
+        surf1 = ax.plot_surface(X, Y, Z, cmap='viridis')
     if options['contour']:
-      contour = ax.contour(X, Y, Z, [k], zdir='z', cmap='coolwarm')
+        contour = ax.contour(X, Y, Z, [k], zdir='z', linewidths=2, colors=['red']) ##cmap='coolwarm')
 
 def cartesianPlot(ax, options):
     x = 0
     y = R_l - Y_OFFSET
     k = aCart(x, y)
     if options['verbose']:
-      print(f"k={k} Cartesian")
+        print(f"k={k}, x={x}, y={y} Cartesian")
 
     if options['apex']:
-      plt.plot(x, y, k, "o", color="magenta", label='cartesian apex')
+        plt.plot(x, y, k, "o", color="magenta", label='cartesian apex')
 
-    x = np.linspace(0, (R_l / 2), NUM_STEPS)
-    y = np.linspace(R_l, (R_l / 2), NUM_STEPS)
+    x = np.linspace(0, R_l, NUM_STEPS)
+    y = np.linspace(0, R_l, NUM_STEPS)
     X, Y = np.meshgrid(x, y)
     Z = aCart(X, Y)
 
     if options['surface']:
-      surf2 = ax.plot_surface(X, Y, Z, cmap='viridis')
+        surf2 = ax.plot_surface(X, Y, Z, cmap='viridis')
     if options['contour']:
-      contour = ax.contour(X, Y, Z, [k], zdir='z', cmap='viridis')
-#      contour = ax.contour(X, Y, Z, zdir='z', offset=ax.get_zlim()[0], cmap='coolwarm')
+      contour = ax.contour(X, Y, Z, [k], zdir='z', colors=['red']) ##cmap='viridis')
+#        contour = ax.contour(X, Y, Z, zdir='z', offset=ax.get_zlim()[0], cmap='coolwarm')
+
+def drawPrism(ax, prismShape):
+    if prismShape.lower() == "hexagonal":
+        APICAL_ANGLE = HEX_ANGLE
+    elif prismShape.lower() == "pentagonal":
+        APICAL_ANGLE = PENT_ANGLE
+    else:
+        raise Exception(f"Unknown prism type: {prismShape} != hexagonal | pentagonal")
+
+    # foci and peak of slow explosive
+    plt.plot(*FOCUS_1, 'ro', label='Focus 1')
+    plt.plot(*FOCUS_2, 'go', label='Focus 2')
+    plt.plot(0, (LENS_OUTER_RADIUS - 100.0), 'b+', label='APEX')
+
+    # draw the hexagonal prism boundaries
+    x1 = LENS_OUTER_RADIUS * np.sin(APICAL_ANGLE)
+    y1 = LENS_OUTER_RADIUS * np.cos(APICAL_ANGLE)
+    x2 = LENS_INNER_RADIUS * np.sin(APICAL_ANGLE)
+    y2 = LENS_INNER_RADIUS * np.cos(APICAL_ANGLE)
+    plt.plot([x1, x2], [y1, y2], 'b-', linewidth=2)
+    plt.plot([-x1, -x2], [y1, y2], 'b-', linewidth=2)
+    theta1 = degrees(np.arctan2(y1, x1))
+    theta2 = degrees(np.arctan2(y1, -x1))
+    if theta2 < theta1:
+        theta2 += 360
+#    arc = Arc((0, 0), LENS_OUTER_DIAMETER, LENS_OUTER_DIAMETER, angle=0, theta1=theta1, theta2=theta2, linewidth=2, color='blue')
+#    ax.add_patch(arc)
+#    arc = Arc((0, 0), LENS_INNER_DIAMETER, LENS_INNER_DIAMETER, angle=0, theta1=theta1, theta2=theta2, linewidth=2, color='blue')
+#    ax.add_patch(arc)
+
 
 def run(options):
-  fig = plt.figure()
-  ax = fig.add_subplot(111, projection='3d')
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-  if options['polar']:
-    polarPlot(ax, options)
+    drawPrism(ax, "hexagonal")
 
-  if options['cartesian']:
-    cartesianPlot(ax, options)
+    if options['polar']:
+        polarPlot(ax, options)
 
-  ax.set_xlabel('X')
-  ax.set_ylabel('Y')
-  ax.set_zlabel('k')
-  plt.show()
+    if options['cartesian']:
+        cartesianPlot(ax, options)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('k')
+    plt.show()
  
 def getOps():
     usage = f"Usage: {sys.argv[0]} [-v] [-p] [-c] [-s] [-C] [-a]"
@@ -155,7 +182,7 @@ def getOps():
         help="Print debug info")
     opts = ap.parse_args().__dict__
     if opts['verbose']:
-      pprint(opts)
+        pprint(opts)
     return opts
 
 if __name__ == '__main__':
