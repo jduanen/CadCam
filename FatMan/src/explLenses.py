8b4d482a583d852ae93cd387a79ab02700f1fc89
Duane Northcutt <duane.northcutt@gmail.com>
2025-03-12 15:49:51 -0700

    Corrected dimensions. Cleaned up.

--- FatMan/src/explLenses.py
+++ FatMan/src/explLenses.py
@@ -26,11 +26,9 @@
 '''
 
 import numpy as np
-from math import asin, cos, degrees, pi, sin, sqrt, tan
+from math import asin, cos, degrees, sin, sqrt
 import matplotlib.pyplot as plt
 from matplotlib.patches import Arc, Polygon
-
-import pdb  ## pdb.set_trace()  #### TMP TMP TMP
 
 
 OUTPUT_FILENAME = "slowExpl.scad"
@@ -38,32 +36,11 @@ OUTPUT_FILENAME = "slowExpl.scad"
 FAST_EXPL = 8050*1000  # mm/s
 SLOW_EXPL = 4900*1000  # mm/s
 
-#### TODO check dimensions for accuracy
-LARGEST_RADIUS = 1500
-LARGEST_DIAMETER = (LARGEST_RADIUS * 2)
-
-LENS_OUTER_RADIUS = 688.975
-LENS_OUTER_DIAMETER = (LENS_OUTER_RADIUS * 2)  # 1377.95
-LENS_HEIGHT = 224.425
-LENS_INNER_RADIUS = (LENS_OUTER_RADIUS - LENS_HEIGHT)  # 465.55
+LENS_OUTER_RADIUS = 690.56
+LENS_OUTER_DIAMETER = (LENS_OUTER_RADIUS * 2)  # 1381.13
+LENS_HEIGHT = 228.60
+LENS_INNER_RADIUS = (LENS_OUTER_RADIUS - LENS_HEIGHT)  # 461.96
 LENS_INNER_DIAMETER = (LENS_INNER_RADIUS * 2)
-LENS_FELT_GAP = 2
-
-BOOSTER_OUTER_RADIUS = 463.55
-BOOSTER_OUTER_DIAMETER = (BOOSTER_OUTER_RADIUS * 2)  # 927.1
-BOOSTER_HEIGHT = 228.6
-BOOSTER_INNER_RADIUS = (BOOSTER_OUTER_RADIUS - BOOSTER_HEIGHT)
-BOOSTER_INNER_DIAMETER = (BOOSTER_INNER_RADIUS * 2)
-BOOSTER_FELT_GAP = 2
-
-INTER_LENS_GAP = 0.5
-
-PUSHER_OUTER_RADIUS = 234.95
-PUSHER_OUTER_DIAMETER = (PUSHER_OUTER_RADIUS * 2)
-
-CORK_THICKNESS = 12.7
-
-GAP = INTER_LENS_GAP
 
 CURVE_APEX = 100.0
 
@@ -111,7 +88,6 @@ def transitionCurve(prismShape):
     apexY = (LENS_OUTER_RADIUS - CURVE_APEX)
     plt.plot(apexX, apexY, "o", color="black", label="Starting point")
     k = func(apexX, apexY)
-    print(f"f({apexX}, {apexY}) = {k}")
 
     minX = 0
     maxX = (LENS_OUTER_RADIUS * sin(APICAL_ANGLE))
@@ -125,9 +101,8 @@ def transitionCurve(prismShape):
     K = func(X, Y)
     if False:
         ax2.contour(X, Y, K, [k], zdir='z', colors=['magenta'])
-        surf2 = ax2.plot_surface(X, Y, K, cmap='viridis')
+#        surf2 = ax2.plot_surface(X, Y, K, cmap='viridis')
     mask = np.isclose(K, k, atol=TOLERANCE)
-    print(f"# Trues: {np.count_nonzero(mask)}")
     X_k = X[mask]
     Y_k = Y[mask]
     if False:
@@ -135,13 +110,9 @@ def transitionCurve(prismShape):
 
     #### FIXME check for points outside the limits
     points = np.column_stack((X_k, Y_k))
-    s1 = points.shape
     points = np.array([p for i, p in enumerate(points) if p not in points[:i]])
-    s2 = points.shape
 #    points = np.insert(points, 0, (apexX, apexY), axis=0)
     points = np.append(points, [(apexX, apexY), (0, minY), (points[0][0], minY)], axis=0)
-    s3 = points.shape
-    print(f"{s1}, {s2}, {s3}")
     polygon = Polygon(points, closed=True, fill=False)
     ax.add_patch(polygon)
     return polygon.get_xy()
@@ -236,68 +207,3 @@ def getOps():
 if __name__ == '__main__':
     opts = getOps()
     run(opts)
-
-
-'''
-def func(y, x, r):
-    return sqrt(x**2 + y**2 - (2 * LENS_OUTER_RADIUS * y) + LENS_OUTER_RADIUS**2) + \
-           ((FAST_EXPL * sqrt(x**2 + y**2)) / SLOW_EXPL) - \
-           (r + ((FAST_EXPL * (1 - r)) / SLOW_EXPL))
-
-# first derivative of 'f'' with respect to 'y'
-def funcPrime(y, x):
-    return (((2 * y) - (2 * LENS_OUTER_RADIUS)) / (2 * sqrt(x**2 + y**2 - (2 * LENS_OUTER_RADIUS * y) + LENS_OUTER_RADIUS**2))) + \
-           ((FAST_EXPL * y)/(SLOW_EXPL * sqrt(x**2 + y**2)))
-
-def newtonMethod(x, r, f, fPrime, y0=0, maxIterations=100, tolerance=1e-6):
-    y = y0
-    for i in range(maxIterations):
-        yNew = y - f(y, x, r) / fPrime(y, x)
-        print(f"{y}, {yNew}, {f(y, x, r)}, {fPrime(y,x)}")
-        if abs(yNew - y) < tolerance:
-            print("Tolerance")
-            return yNew
-        y = yNew
-        return 0
-    print("MaxIterations")
-    return y  # Return the last computed value if maxIterations is reached
-
-
-    tolerance = 1.0
-
-    r = np.linspace(0, (LENS_OUTER_RADIUS / 2), 100)  #### 1000
-    theta = np.linspace(0, -(np.pi / 2), 100)  #### 1000
-    R, THETA = np.meshgrid(r, theta)
-
-    Z = aPolar(R, THETA)
-
-    mask = np.abs(Z - k) < tolerance
-    pdb.set_trace()  #### TMP TMP TMP
-    X, Y = toCart(R[mask], THETA[mask], LENS_OUTER_RADIUS)
-    plt.scatter(X, Y, s=1)
-
-#    surf1 = ax.plot_surface(X, Y, Z, cmap='viridis')
-#    contour = ax.contour(X, Y, Z, [k], zdir='z', cmap='coolwarm')
-
-def xxxTransitionCurve():
-    END_TOLERANCE = 0.1
-    MAX_X = int((LENS_OUTER_RADIUS * sin(HEX_ANGLE)))
-    X = [x for x in range(0, MAX_X)]
-    y = (LENS_OUTER_RADIUS - 100.0)
-    Y = [y]
-    r = 0.854856853
-    plt.plot(X[0], Y[0], "o", color="black", label="Start")
-    for x in X[1:]:
-        y = newtonMethod(x, r, func, funcPrime, y)
-        Y.append(y)
-        endY = (x * tan((pi / 2) - HEX_ANGLE))
-        plt.plot(x, endY, "+", color="cyan", label="End")
-        if abs(y - endY) < END_TOLERANCE:
-            print("TERM COND")
-            break;
-    plt.plot(X, Y, '*', color="hotpink", label='Hex Transisition')
-    negX = [-x for x in range(1, MAX_X)]
-    plt.plot(negX, Y[1:], '*', color="hotpink", label='Hex Transition')
-    #plt.legend()
-
-'''
