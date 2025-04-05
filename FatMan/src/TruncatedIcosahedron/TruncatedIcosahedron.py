5565141effebb1d2eade967ac4005967cfb11ad6
Duane Northcutt <duane.northcutt@gmail.com>
2025-04-05 13:20:17 -0700

    Added/fixed methods. Fixed vertices.

--- FatMan/src/TruncatedIcosahedron/TruncatedIcosahedron.py
+++ FatMan/src/TruncatedIcosahedron/TruncatedIcosahedron.py
@@ -59,20 +59,7 @@ class TruncatedIcosahedron():
             self.vertices.append((x, y, z))
         self.vertices = self.scale * np.array(self.vertices)
 
-        self.pentagonalFaces = [[8, 50, 26, 24, 48, 8],  # Pentagon 0
-                                [0, 36, 12, 16, 40, 0],
-                                [2, 38, 14, 18, 42, 2],
-                                [9, 49, 25, 27, 51, 9],
-                                [4, 44, 20, 32, 56, 4],
-                                [5, 57, 33, 21, 45, 5],
-                                [7, 59, 35, 23, 47, 7],
-                                [1, 41, 17, 13, 37, 1],
-                                [3, 39, 15, 19, 43, 3],
-                                [6, 58, 34, 22, 46, 6],
-                                [10, 54, 30, 28, 52, 10],
-                                [11, 55, 31, 29, 53, 11]  # Pentagon 11
-                                ]
-        self.hexagonalFaces = [[0, 2, 38, 26, 24, 36, 0],     # Hexagon 0
+        self.hexagonalFaces = [[0, 2, 38, 26, 24, 36, 0],     #0: Hexagon
                                [0, 2, 42, 30, 28, 40, 0],
                                [12, 36, 24, 48, 20, 44, 12],
                                [14, 45, 21, 50, 26, 38, 14],
@@ -91,24 +78,52 @@ class TruncatedIcosahedron():
                                [10, 52, 22, 34, 53, 11, 10],
                                [10, 11, 55, 35, 23, 54, 10],
                                [19, 43, 31, 55, 35, 59, 19],
-                               [17, 58, 34, 53, 29, 41, 17]     # Hexagon 19
+                               [17, 58, 34, 53, 29, 41, 17]     #19: Hexagon
                                ]
+        self.hexCount = len(self.hexagonalFaces)
+
+        self.pentagonalFaces = [[8, 50, 26, 24, 48, 8],  #20: Pentagon
+                                [0, 36, 12, 16, 40, 0],
+                                [2, 38, 14, 18, 42, 2],
+                                [9, 49, 25, 27, 51, 9],
+                                [4, 44, 20, 32, 56, 4],
+                                [5, 57, 33, 21, 45, 5],
+                                [7, 59, 35, 23, 47, 7],
+                                [1, 41, 17, 13, 37, 1],
+                                [3, 39, 15, 19, 43, 3],
+                                [6, 58, 34, 22, 46, 6],
+                                [10, 54, 30, 28, 52, 10],
+                                [11, 55, 31, 29, 53, 11]  #31: Pentagon
+                                ]
+        self.pentCount = len(self.pentagonalFaces)
         self.faces = [*self.hexagonalFaces, *self.pentagonalFaces]
 
-    def getFaceNumbers(self):
-        return self.faces
+    def hexagonalFaceCount(self):
+        return self.hexCount
 
-    def isFacePentagonal(self):
-        return faceNumber in self.pentagonalFaces
+    def pentagonalFaceCount(self):
+        return self.pentCount
 
     def isFaceHexagonal(self, faceNumber):
-        return faceNumber in self.hexagonalFaces
+        return faceNumber >= 0 and faceNumber < self.hexCount
 
-    def getPentagonalFaceNumbers(self):
+    def isFacePentagonal(self, faceNumber):
+        return faceNumber >= self.hexCount and faceNumber < (self.hexCount + self.pentCount)
+
+    def getHexagonalFaces(self):
+        return self.hexagonalFaces
+
+    def getPentagonalFaces(self):
         return self.pentagonalFaces
 
+    def getFaceNumbers(self):
+        return list(range(0, (self.hexCount + self.pentCount)))
+
     def getHexagonalFaceNumbers(self):
-        return self.hexagonalFaces
+        return list(range(0, self.hexCount))
+
+    def getPentagonalFaceNumbers(self):
+        return list(range(self.hexCount, (self.hexCount + self.pentCount)))
 
     def getFaceVertices(self, faceNumber):
         return self.vertices[self.faces[faceNumber]]
@@ -118,6 +133,9 @@ class TruncatedIcosahedron():
 
     def getFaceNormal(self, faceNumber):
         pass  #### TBD
+
+    def getFaces(self):
+        return self.faces
 
     def getVertices(self):
         return self.vertices
