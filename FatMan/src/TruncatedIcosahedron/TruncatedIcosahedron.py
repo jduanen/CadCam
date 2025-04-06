6726313e9577a45d9b39594bf0f7e89f36e071d9
Duane Northcutt <duane.northcutt@gmail.com>
2025-04-05 17:03:01 -0700

    Fixed face directions. Added getFaceCenter() and getFaceNormal().

--- FatMan/src/TruncatedIcosahedron/TruncatedIcosahedron.py
+++ FatMan/src/TruncatedIcosahedron/TruncatedIcosahedron.py
@@ -11,6 +11,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+#### FIXME make methods consistently return either lists of floats or np.arrays
+
 class TruncatedIcosahedron():
     def __init__(self, scale=1.0):
         self.scale = scale
@@ -60,40 +62,40 @@ class TruncatedIcosahedron():
         self.vertices = self.scale * np.array(self.vertices)
 
         self.hexagonalFaces = [[0, 2, 38, 26, 24, 36],   #0: Hexagon
-                               [0, 2, 42, 30, 28, 40],
+                               [40, 28, 30, 42, 2, 0],
                                [12, 36, 24, 48, 20, 44],
                                [14, 45, 21, 50, 26, 38],
                                [8, 50, 21, 33, 51, 9],
                                [8, 9, 49, 32, 20, 48],
                                [4, 6, 46, 16, 12, 44],
-                               [5, 7, 47, 18, 14, 45],
-                               [13, 37, 25, 49, 32, 56],
+                               [45, 14, 18, 47, 7, 5],
+                               [56, 32, 49, 25, 37, 13],
                                [1, 3, 43, 31, 29, 41],
-                               [1, 3, 39, 27, 25, 37],
-                               [15, 57, 33, 51, 27, 39],
+                               [37, 25, 27, 39, 3, 1],
+                               [39, 27, 51, 33, 57, 15],
                                [4, 56, 13, 17, 58, 6],
-                               [5, 57, 15, 19, 59, 7],
+                               [7, 59, 19, 15, 57, 5],
                                [18, 42, 30, 54, 23, 47],
-                               [16, 40, 28, 52, 22, 46],
+                               [46, 22, 52, 28, 40, 16],
                                [10, 52, 22, 34, 53, 11],
                                [10, 11, 55, 35, 23, 54],
-                               [19, 43, 31, 55, 35, 59],
-                               [17, 58, 34, 53, 29, 41]  #19: Hexagon
+                               [59, 35, 55, 31, 43, 19],
+                               [41, 29, 53, 34, 58, 17]  #19: Hexagon
                                ]
         self.hexCount = len(self.hexagonalFaces)
 
-        self.pentagonalFaces = [[8, 50, 26, 24, 48],  #20: Pentagon
+        self.pentagonalFaces = [[48, 24, 26, 50, 8],  #20: Pentagon
                                 [0, 36, 12, 16, 40],
-                                [2, 38, 14, 18, 42],
-                                [9, 49, 25, 27, 51],
+                                [42, 18, 14, 38, 2],
+                                [51, 27, 25, 49, 9],
                                 [4, 44, 20, 32, 56],
                                 [5, 57, 33, 21, 45],
-                                [7, 59, 35, 23, 47],
+                                [47, 23, 35, 59, 7],
                                 [1, 41, 17, 13, 37],
                                 [3, 39, 15, 19, 43],
                                 [6, 58, 34, 22, 46],
                                 [10, 54, 30, 28, 52],
-                                [11, 55, 31, 29, 53]  #31: Pentagon
+                                [52, 29, 31, 55, 11]  #31: Pentagon
                                 ]
         self.pentCount = len(self.pentagonalFaces)
         self.faces = [*self.hexagonalFaces, *self.pentagonalFaces]
@@ -132,11 +134,17 @@ class TruncatedIcosahedron():
         return vertices
 
     def getFaceCenter(self, faceNumber):
-        vertices = np.array(self.getFaceVertices(faceNumber))
-        return np.mean(vertices[:-1], axis=0)
+        vertices = self.getFaceVertices(faceNumber)
+        return np.mean(vertices, axis=0)
 
     def getFaceNormal(self, faceNumber):
-        pass  #### TBD
+        vertices = self.getFaceVertices(faceNumber)
+        center = self.getFaceCenter(faceNumber)
+        v1 = vertices[0] - center
+        v2 = vertices[1] - center
+        normal = np.cross(v1, v2)
+        normal /= np.linalg.norm(normal)
+        return normal
 
     def getFaces(self):
         return self.faces
